ef57531f152d3c13a782d1f15d9ac0031e35487d
Felipe Macedo <felipebmacedo.91@gmail.com>
2023-12-27 20:21:22 -0300

    add Api domain with tests

---
+++ src/main/java/br/com/catalog/domain/Api.java
@@ -0,0 +1,60 @@
+package br.com.catalog.domain;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.UUID;
+
+public class Api {
+    private String id;
+    private String name;
+    private String description;
+
+    public Api() {
+    }
+
+    private Api(String id, String name, String description) {
+        validateName(name);
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public static Api with(String name, String description) {
+        return new Api(UUID.randomUUID().toString(), name, description);
+    }
+
+    public static Api with(String id, String name, String description) {
+        return new Api(id, name, description);
+    }
+
+    private static void validateName(String name) {
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException("Name is required");
+        }
+
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("Api`s name has to be greater than 3 characters");
+        }
+    }
+
+    public void changeName(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    public void changeDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/test/java/br/com/catalog/domain/ApiTest.java
@@ -0,0 +1,153 @@
+package br.com.catalog.domain;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class ApiTest {
+
+    @Test
+    public void createApiTest() {
+        final var expectedName = "Api Test";
+        final var expectedDescription = "My first Api";
+
+        Api api = Api.with(expectedName, expectedDescription);
+
+        Assertions.assertEquals(expectedName, api.getName());
+        Assertions.assertEquals(expectedDescription, api.getDescription());
+    }
+
+    @Test
+    public void createApiWithIdTest() {
+        final var expectedId = UUID.randomUUID().toString();
+        final var expectedName = "Api Test";
+        final var expectedDescription = "My first Api";
+
+        Api api = Api.with(expectedId, expectedName, expectedDescription);
+
+        Assertions.assertEquals(expectedId, api.getId());
+        Assertions.assertEquals(expectedName, api.getName());
+        Assertions.assertEquals(expectedDescription, api.getDescription());
+    }
+
+    @Test
+    public void createApiWithEmptyDescriptionTest() {
+        final var expectedName = "Api Test";
+        final var expectedDescription = "";
+
+        Api api = Api.with(expectedName, expectedDescription);
+
+        Assertions.assertEquals(expectedName, api.getName());
+        Assertions.assertEquals("", api.getDescription());
+    }
+
+    @Test
+    public void changeApiNameTest() {
+        final var expectedName = "New Name";
+        final var expectedDescription = "Old Description";
+
+        Api api = Api.with("Old Name", expectedDescription);
+
+        api.changeName(expectedName);
+
+        Assertions.assertEquals(expectedName, api.getName());
+        Assertions.assertEquals(expectedDescription, api.getDescription());
+    }
+
+    @Test
+    public void changeApiDescriptionTest() {
+        final var expectedName = "Old Name";
+        final var expectedDescription = "New Description";
+
+        Api api = Api.with(expectedName, "Old Description");
+
+        api.changeDescription(expectedDescription);
+
+        Assertions.assertEquals(expectedName, api.getName());
+        Assertions.assertEquals(expectedDescription, api.getDescription());
+    }
+
+    @Test
+    public void changeApiDescriptionWithEmptyTest() {
+        final var expectedName = "Old Name";
+        final var expectedDescription = "";
+
+        Api api = Api.with(expectedName, "Old Description");
+
+        api.changeDescription(expectedDescription);
+
+        Assertions.assertEquals(expectedName, api.getName());
+        Assertions.assertEquals(expectedDescription, api.getDescription());
+    }
+
+    @Test
+    public void createApiWithEmptyNameTest() {
+        final var expectedExceptionMessage = "Name is required";
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Api.with("", "My first API");
+        });
+
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    public void createApiWithNullNameTest() {
+        final var expectedExceptionMessage = "Name is required";
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Api.with(null, "My first API");
+        });
+
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    public void createApiWithLessThanThreeCharsNameTest() {
+        final var expectedExceptionMessage = "Api`s name has to be greater than 3 characters";
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Api.with("Ap", "My first API");
+        });
+
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    public void changeApiWithLessThanThreeCharsNameTest() {
+        final var expectedExceptionMessage = "Api`s name has to be greater than 3 characters";
+        Api api = Api.with("Api Test", "My first API");
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            api.changeName("Ap");
+        });
+
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    public void changeApiWithNullNameTest() {
+        final var expectedExceptionMessage = "Name is required";
+        Api api = Api.with("Api Test", "My first API");
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            api.changeName(null);
+        });
+
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    public void changeApiWithEmptyNameTest() {
+        final var expectedExceptionMessage = "Name is required";
+        Api api = Api.with("Api Test", "My first API");
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            api.changeName("");
+        });
+
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+}
